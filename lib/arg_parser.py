65b1d15999513a0fbb67c29b030ec1858412da93
Avor Sky <1737000@gmail.com>
2022-06-28 18:32:21 +0300

    Simplified the arg_parser flow

--- lib/arg_parser.py
+++ lib/arg_parser.py
@@ -1,15 +1,15 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class ParsedTaskData:
     description:    str
     due_time:       str
-    project_name:   str
 
 
-def parse_input(list_args: list[str]) -> ParsedTaskData:
-    def _show_help() -> str:
+def parse_input(list_args: list[str]) -> Optional[ParsedTaskData]:
+    def _show_help():
         print("Add a new task: dodo To water the flowers")
 
     def _called_with_no_args() -> bool:
@@ -25,31 +25,12 @@ def parse_input(list_args: list[str]) -> ParsedTaskData:
         return ' '.join(list_args[1:]).capitalize()
 
     def _parse_due_time() -> str:
-        return input('Due time for this task: ').capitalize()
+        return input('Due: ').capitalize()
     
-    def _parse_project_name() -> str:
-        return input('Project name: ').capitalize()
-    
-    def _parsed_task_data() -> ParsedTaskData:
-        return ParsedTaskData(description=' '.join(list_args[1:]))
-    
-    def _user_confirmation() -> bool:
-        no_decision = input('Enter to Save (type anything else to Discard): ')
-        if no_decision:
-            print('Task discarded.')
-            return False
-        else:
-            print('Task saved.')
-            return True
-
 
     if _called_with_no_args() or _user_needs_help():
         _show_help()
     else:
-        parsed_description  = _parse_description()
-        parsed_due_time     = _parse_due_time()
-        parsed_project      = _parse_project_name()
-        if _user_confirmation():
-            return ParsedTaskData(description=parsed_description,
-                                  due_time=parsed_due_time,
-                                  project_name=parsed_project)
+        return ParsedTaskData(description=_parse_description(), 
+                              due_time=_parse_due_time())
+    return None
