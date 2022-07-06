233d84c258f94dfae4dc5a2a40bf161356d9c6ab
Avor Sky <1737000@gmail.com>
2022-07-06 17:39:56 +0300

    Added the string formatter class to handle low level output functionality

--- lib/view_controller.py
+++ lib/view_controller.py
@@ -1,76 +1,88 @@
 from datamodel import Task
+from config import OUTPUT_WIDTH, HEADERS, HEADER_DIV_PCT
 
 
-width = 68 
-header_div_pct = {
-                        'num': 8,
-                        'description': 55,
-                        'due_date': 20,
-                        'priority': 17
-                    }
+OUTPUT_WIDTH    = OUTPUT_WIDTH 
+HEADERS         = HEADERS
+HEADER_DIV_PCT  = HEADER_DIV_PCT
+
+
+class StringFormatter():
+    '''Provides interface to conform dividers and other scrings 
+    to follow the same unified format to be used as output.'''
+
+    def __init__(cls, str_width: int = 80, 
+                 headers: list[str] = ['#', 'Task description', 'Due', 'Priority'], 
+                 headers_div_pct: list[int]= [5, 60, 18, 17]) -> None:
+        cls.outer_width     = str_width 
+        cls.inner_width     = str_width-2
+        cls.headers         = headers
+        cls.headers_div_pct = headers_div_pct
+
+
+    def generate_divider(cls, border_char: str = '+', filler_char: str = '-') -> str:
+        return f'{border_char}{filler_char*cls.inner_width}{border_char}'
+    
+    
+    def generate_header(cls, border_char: str = '|') -> str:
+        header_str = ''
+        space = ' '
+
+        for header, div_pct in zip(cls.headers, cls.headers_div_pct):
+            current_div = int(cls.inner_width / 100 * div_pct)
+            current_str = f' {header}{space * current_div}'
+            header_str = header_str + f'{current_str[:current_div]}|'
+
+        return f'{border_char}{header_str[: cls.inner_width] + border_char}'
+    
+
+    def generate_task(cls, task: Task, task_nr: int, border_char: str = '|') -> str:
+        task_str = ''
+        task_items = [task_nr, task.description, task.due_time, task.priority]
+        space = ' '
+
+        for item, div_pct in zip(task_items, cls.headers_div_pct):
+            current_div = int(cls.inner_width / 100 * div_pct)
+            current_str = f' {item}{space * current_div}'
+            task_str = task_str + f'{current_str[:current_div]}|'
+
+        return f'{border_char}{task_str[: cls.inner_width] + border_char}'
+        pass
+    
+    
+    def generate_placeholder(cls, border_char: str = '|') -> str:
+        message = " Such empty. 'dodo To water flowers' will add a new task."
+        return f'{border_char}{(message + " "*cls.inner_width)[:cls.inner_width]}{border_char}'
 
 
 def beautify_output(tasks: list[Task], message: str='') -> None:
-    column_width = {
-                            'num': round(width/100* header_div_pct['num']),
-                            'description': round(width/100*header_div_pct['description']),
-                            'due_date': round(width/100*header_div_pct['due_date']),
-                            'priority': round(width/100*header_div_pct['priority'])
-                        }
-    adjusted_width = width+2 # Adjusted to account for borders
-    horizontal_line =   '+' + '-' * adjusted_width + '+'
-    line_prefix = '| '
-    empty_line = '|' +  ' ' * adjusted_width + '|'
 
+    output_formatter = StringFormatter(str_width        =OUTPUT_WIDTH,
+                                       headers          =HEADERS, 
+                                       headers_div_pct  =HEADER_DIV_PCT)
 
     def _print_headers():
-        headers = {
-            'num': f'| #{" "*column_width["num"]}'[:column_width['num']],
-            'description': f'| Description{" "*column_width["description"]}'[:column_width['description']],
-            'due_date': f'| Due{" "*column_width["due_date"]}'[:column_width['due_date']],
-            'priority': f'| Priority{" "*column_width["priority"]}'[:column_width['priority']]
-            }
-        space_adjustment = width - (len(headers['num'])+len(headers['description'])+len(headers['due_date'])+len(headers['priority']))
-
-        print(horizontal_line)
-        print(headers['num'], headers['description'], headers['due_date'], headers['priority']+' '*space_adjustment+'|')
-        print(horizontal_line)
-        print(empty_line)
+        print(output_formatter.generate_divider())
+        print(output_formatter.generate_header())
+        print(output_formatter.generate_divider())
+        print(output_formatter.generate_divider(border_char='|', filler_char=' '))
     
     def _print_task_placeholder():
-        print(line_prefix, end='')
-        print(('Such empty. There are no tasks to show. ' + ' '*width)[:width] + ' ', end='')
-        print(line_prefix)
+        print(output_formatter.generate_placeholder())
     
     def _print_task(task: Task, task_number: int):
-
-        def _task_to_string(task: Task, task_number: int) -> str:
-            task_num = str(task_number)
-            task_data = {
-                'num': f'| {task_num}{" "*column_width["num"]}'[:column_width['num']+1],
-                'description': f'| {task.description}{" "*column_width["description"]}'[:column_width['description']+1],
-                'due_date': f'| {task.due_time}{" "*column_width["due_date"]}'[:column_width['due_date']+1],
-                'priority': f'| {task.priority}{" "*column_width["priority"]}'[:column_width['priority']]
-                }
-            space_adjustment = width - (len(task_data['num'])+len(task_data['description'])+len(task_data['due_date'])+len(task_data['priority']))
-
-            return f"{task_data['num']}{task_data['description']}{task_data['due_date']}{task_data['priority']}{' '*space_adjustment}|"
-
-
-        print(_task_to_string(task=task, task_number=task_number))
+        print(output_formatter.generate_task(task=task, task_nr=task_number))
     
     def _print_message():
-        print(empty_line)
-        print(horizontal_line)
-        print(f'{message}')
+        print(output_formatter.generate_divider(border_char='|', filler_char=' '))
+        print(output_formatter.generate_divider())
+        print(message)
     
 
     _print_headers()
-
     if not tasks:
         _print_task_placeholder()
     else:
         for number, task in enumerate(tasks, start=1):
             _print_task(task=task, task_number=number)
-
     _print_message()
